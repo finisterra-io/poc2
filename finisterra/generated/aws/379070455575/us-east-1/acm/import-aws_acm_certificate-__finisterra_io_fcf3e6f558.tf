230df4049bda5fad0094c224b4b5e6982b8e3d70
daniel-finisterra <daniel-finisterra@users.noreply.github.com>
2023-12-07 02:34:04 +0000

    Automatic scan for acm.aws.379070455575.us-east-1

---
+++ finisterra/generated/aws/379070455575/us-east-1/acm/import-aws_acm_certificate-__finisterra_io_fcf3e6f558.tf
@@ -0,0 +1,5 @@
+import {
+  id = "arn:aws:acm:us-east-1:379070455575:certificate/7dc2d279-2c12-49ce-901a-a4acaafbb34f"
+  to = module.aws_acm_certificate-__finisterra_io_fcf3e6f558.aws_acm_certificate.this[0]
+}
+
