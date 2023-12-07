762e345fed4befb0ce0732a834a891a48b728257
daniel-finisterra <daniel-finisterra@users.noreply.github.com>
2023-12-07 02:33:51 +0000

    Automatic scan for s3.aws.379070455575.us-east-1

---
+++ finisterra/generated/aws/379070455575/us-east-1/s3/import-aws_s3_bucket-finisterra-aws-connect_a6570aa3d7.tf
@@ -0,0 +1,30 @@
+import {
+  id = "finisterra-aws-connect"
+  to = module.aws_s3_bucket-finisterra-aws-connect_a6570aa3d7.aws_s3_bucket.this[0]
+}
+
+import {
+  id = "finisterra-aws-connect"
+  to = module.aws_s3_bucket-finisterra-aws-connect_a6570aa3d7.aws_s3_bucket_versioning.this[0]
+}
+
+import {
+  id = "finisterra-aws-connect"
+  to = module.aws_s3_bucket-finisterra-aws-connect_a6570aa3d7.aws_s3_bucket_server_side_encryption_configuration.this[0]
+}
+
+import {
+  id = "finisterra-aws-connect"
+  to = module.aws_s3_bucket-finisterra-aws-connect_a6570aa3d7.aws_s3_bucket_request_payment_configuration.this[0]
+}
+
+import {
+  id = "finisterra-aws-connect"
+  to = module.aws_s3_bucket-finisterra-aws-connect_a6570aa3d7.aws_s3_bucket_public_access_block.this[0]
+}
+
+import {
+  id = "finisterra-aws-connect"
+  to = module.aws_s3_bucket-finisterra-aws-connect_a6570aa3d7.aws_s3_bucket_ownership_controls.this[0]
+}
+
